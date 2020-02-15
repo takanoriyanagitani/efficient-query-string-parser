3238f5f15b5440a1d7d9b2e49a1209f938be16a9
yanagitani <takanori.yanagitani@gmail.com>
2020-02-16 05:56:02 +0900

    trying to test...

---
+++ bytes.h
@@ -0,0 +1,6 @@
+#include <stdint.h>
+
+typedef struct {
+  size_t   size;
+  uint8_t* data;
+} bytes_t;
---
+++ skip_amp_test.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+#include <string.h>
+
+#include "skip_amp.h"
+
+int test(const char* query_string, const char* expected){
+  char buf[1024] = {0};
+  if(NULL != query_string) strncpy(buf, query_string, 1023);
+  bytes_t s = {0};
+  s.size = strlen(buf);
+  s.data = (uint8_t*)buf;
+
+  size_t offset = 0;
+  int rc = eparser_skip_amp(s, &offset);
+  snprintf(buf, 1024, "rc:%d,offset:%ld", rc, offset);
+  return 0 == strncmp(buf, expected, 1023) ? 0 : 0 < fprintf(stdout, "actual:%s != expected: %s\n", buf, expected);
+}
+
+int main(int argc, char** argv){
+  switch(argc){
+    default: return test(NULL,    "rc:0,offset:0");
+    case  3: return test(argv[1], argv[2]);
+  }
+}
