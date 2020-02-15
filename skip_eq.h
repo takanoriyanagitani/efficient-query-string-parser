467ec36dbcecbaa399d56ef8c280242f2fb4fe70
yanagitani <takanori.yanagitani@gmail.com>
2020-02-16 06:25:53 +0900

    skip eq

---
+++ skip_eq.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "bytes.h"
+
+int eparser_skip_eq(bytes_t query_string, size_t* offset){
+  if(NULL == query_string.data) return 0;
+  if(query_string.size < 1)     return 0;
+  for(; *offset < query_string.size; (*offset)++){
+    switch(query_string.data[*offset]){
+      default: continue;
+      case '=': return 1;
+    }
+  }
+  return 0;
+}
